df3ef68bf7a1e4834dbd234560e4e9f6b1666c7f
Luke Haines <lukehaines2@gmail.com>
2020-09-22 18:20:50 +0100

    add flexbox compromised solution

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import './App.css';
 
-const colours = ["lightblue", "lightgrey", "orange", "silver", "lightgreen", "hotpink"];
+
+const gridCreator = () => {
+  const colours = ["lightblue", "lightgrey", "orange", "silver", "lightgreen", "hotpink"];
+  return (
+    colours.map((colour, idx) => (
+      <div key={colour[idx]} className="card">
+        <div className="thumbnail" style={{ background: colour }}></div>
+        <div className="text">
+          <div className="title">I am a card Title</div>
+          <div className="subheader">I am some random smaller text talking about the image!</div>
+        </div>
+      </div>
+    ))
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <div className="grid-container">
-        {colours.map((colour, idx) => (
-          <div key={colour[idx]} className="card">
-            <div className="thumbnail" style={{ background: colour }}></div>
-            <div className="text">
-              <div className="title">I am a card Title</div>
-              <div className="subheader">I am some random smaller text talking about the image!</div>
-            </div>
-          </div>
-        ))}
+        {gridCreator()}
+      </div>
+      <div className="spacer"></div>
+      <div className="flex-container">
+        {gridCreator()}
+        <div className="blank"></div>
       </div>
     </div>
   );
